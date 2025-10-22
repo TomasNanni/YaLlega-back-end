00d89ff895fc0dee2cee751606b00168bb6887af
Tomas Nanni <tomibasquet04@gmail.com>
2025-10-22 20:32:32 -0300

    entidad user agregado

--- YaLlega/Entities/User.cs
+++ YaLlega/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace YaLlega.Entities
 {
     internal class User
     {
-
-    }
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string lastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+        public string address { get; set; }
+    };
 }
